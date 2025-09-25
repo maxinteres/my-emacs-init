3909cbebcea2ba9cfb05a457be15498324f47579
maxinteres <zhumax@me.com>
2025-09-25 13:45:29 +0800

    9/25 failed

--- LISP/init-org.el
+++ LISP/init-org.el
@@ -1,9 +1,7 @@
 ;; -*- lexical-binding: t -*-
 (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
 (add-hook 'org-mode-hook (lambda () (company-mode -1)))
-(require 'org-tempo)
 (setq org-startup-folded t)
-(setq org-hide-block-startup t) 
 (setq org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "BUG(g)" "TRY(y)" "SUS(s)""|" "FIXED(f)" "FAILED(l)" "DONE(d)")))
 (setq org-todo-keyword-faces '(("BUG" . "tomato")
 			       ("WAIT" . "cornflower blue")
