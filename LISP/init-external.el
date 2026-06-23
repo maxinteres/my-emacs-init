c224256c3b01854e345e70cdd381780aa2f96142
meigesy <zhumax@me.com>
2026-06-23 23:07:19 +0800

    6/23 done with init-external.el

--- LISP/init-external.el
+++ LISP/init-external.el
@@ -22,11 +22,12 @@
   :bind (("C-c a" . aidermacs-transient-menu))
   :config
   ; Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
-  (setenv "ANTHROPIC_API_KEY" "sk-...")
+  ; (setenv "ANTHROPIC_API_KEY" "sk-...")
   ; defun my-get-openrouter-api-key yourself elsewhere for security reasons
-  (setenv "OPENROUTER_API_KEY" (my-get-openrouter-api-key))
+  ; (setenv "OPENROUTER_API_KEY" (my-get-openrouter-api-key))
   :custom
   ; See the Configuration section below
+  (aidermacs-args '("--restore-chat-history"))
   (aidermacs-default-chat-mode 'architect)
-  (aidermacs-default-model "sonnet"))
+  (aidermacs-default-model "deepseek"))
 (provide 'init-external)
